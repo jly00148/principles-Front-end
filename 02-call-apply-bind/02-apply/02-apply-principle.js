30b32c2a0efc6371aedc785e5ee46e23d27d622b
jiangliaoyuan <453825818@qq.com>
2021-10-24 03:01:22 +0800

    apply-principle

--- 02-call-apply-bind/02-apply/02-apply-principle.js
+++ 02-call-apply-bind/02-apply/02-apply-principle.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jly
  * @Date: 2021-10-22 04:10:27
- * @LastEditTime: 2021-10-22 04:33:28
+ * @LastEditTime: 2021-10-24 03:00:25
  * @FilePath: \principles-Front-end\02-call-apply-bind\02-apply\02-apply-principle.js
  */
 var obj = {name:'jly'}
@@ -12,11 +12,16 @@ function Foo (age,major){
     console.log(major);
 }
 
-Function.prototype.newApply = function (_this,rest){
-    
-    if(!_this) _this = window;//在浏览器中是window，在node环境不是window而是空对象
-    // console.log(_this,this);// obj/window,Foo
+Function.prototype.newApply = function (){
 
+    let [_this,rest] = arguments;
+    
+    // 如果没传第一个参数，判断this指向
+    if(!_this){
+        // 如果成立说明_this是global(node非严格环境)，否则是window；
+        _this = typeof window === 'undefined' ? global : window;
+    }
+    
     (_this.fn = this) && _this.fn(...rest);
 
     // 调用完没用了，删除属性
